ec206f92b29a3c4801d954f1419bcb0b4eb1c8ef
erika21akire <5100162@mypillar7uwc.acza>
2023-12-12 15:34:32 +0200

    Submitting Assignment 3

---
+++ Assignment_3/RealWorldObjects2Sln/RealWorldObjects2/Designer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealWorldObjects2
+{
+    internal class Designer
+    {
+        // Variables
+        string Brand;
+        double Price;
+        string Material;
+        string HasColor;
+        string Collection;
+
+
+        // Designer method
+        public Designer(string brand, double price, string material, string color, string collection)
+
+        {
+            this.Brand = brand;
+            this.Price = price;
+            this.Material = material;
+            this.HasColor = color;
+            this.Collection = collection;
+        }
+
+        public string GetBrand()
+        { return this.Brand; }
+        public double GetPrice()
+        { return this.Price; }
+        public string GetMaterial()
+        { return this.Material; }
+        public string GetColor()
+        { return this.HasColor; }
+        public string GetCollection()
+        { return this.Collection; }
+
+        public void DisplayDetails()
+        {
+            Console.WriteLine("Brand: " + Brand);
+            Console.WriteLine("Price: R" + Price);
+            Console.WriteLine("Material: " + Material);
+            Console.WriteLine("Color: " + HasColor);
+            Console.WriteLine("Collection: " + Collection);
+        }
+    }
+}
---
+++ Assignment_3/RealWorldObjects2Sln/RealWorldObjects2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.Design;
+
+namespace RealWorldObjects2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Create an instance of DesignerBag
+            Designer Versace = new Designer("Versace", 76800, "Leather", "Black", "La Medusa");
+            
+            Designer SteveMadden = new Designer("Steve Madden", 1899, "Leather", "Black & Clear", "Bonang Exclusive");
+
+            Designer Aldo = new Designer("Aldo ", 1499, "Synthetic", "White & Gold", "HAOACAN");
+
+            // Call the DisplayDetails method to print the bag details
+            Versace.DisplayDetails();
+        }
+    }
+}
